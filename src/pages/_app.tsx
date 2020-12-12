d8dd85807761f245d178109ffcdf3f8918b58eae
YoungeonLee <58812396+YoungeonLee@users.noreply.github.com>
2020-12-12 20:32:13 +0900

    fetch backend to prevent sleep

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { AppProps } from 'next/app'
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 import theme from '../theme'
+import { useEffect } from 'react'
 
 const client = new ApolloClient({
   uri: process.env.NEXT_PUBLIC_BACKEND_URL + '/graphql',
@@ -11,7 +12,20 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 })
 
+function fetchBackend() {
+  fetch(process.env.NEXT_PUBLIC_BACKEND_URL!).then(() =>
+    console.log('fetched backend')
+  )
+}
+
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    fetchBackend()
+    setInterval(() => {
+      fetchBackend()
+    }, 1000 * 60 * 29)
+  }, [])
+
   return (
     <ApolloProvider client={client}>
       <ChakraProvider theme={theme}>
