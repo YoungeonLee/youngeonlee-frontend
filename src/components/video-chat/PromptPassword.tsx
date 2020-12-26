5f7cb4e9d4b689167168980d5ef7641796edad96
YoungeonLee <58812396+YoungeonLee@users.noreply.github.com>
2020-12-26 12:56:28 +0900

    fix build error

--- src/components/video-chat/PromptPassword.tsx
+++ src/components/video-chat/PromptPassword.tsx
@@ -77,7 +77,7 @@ export default function PromptPassword({
             // }
           }}
         >
-          {(props) => (
+          {() => (
             <Form>
               <ModalBody>
                 <CustomInputField
