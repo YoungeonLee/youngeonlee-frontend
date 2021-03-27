a065d24f5185770ef81a3da0f13f7d6fd2b94591
YoungeonLee <58812396+YoungeonLee@users.noreply.github.com>
2021-03-27 18:37:19 +0900

    debug

--- src/pages/video-chat/index.tsx
+++ src/pages/video-chat/index.tsx
@@ -1,4 +1,4 @@
-import { Heading, Container, Center } from '@chakra-ui/react'
+import { Heading, Container, Center, Flex } from '@chakra-ui/react'
 import { DarkModeSwitch } from '../../components/shared/DarkModeSwitch'
 import CreateRoomForm from '../../components/video-chat/CreateRoomForm'
 import Rooms from '../../components/video-chat/Rooms'
@@ -9,10 +9,20 @@ import useHasMounted from '../../utils/useHasMounted'
 export default function VideoChat() {
   const { loading, error, data } = useRoomsQuery()
   if (!useHasMounted()) return null
-  if (loading) return <p>Loading...</p>
+  if (loading) {
+    return (
+      <Flex justify="center" align="center" height="100vh">
+        Loading...
+      </Flex>
+    )
+  }
   if (error) {
     console.log(error)
-    return <p>Error!</p>
+    return (
+      <Flex justify="center" align="center" height="100vh">
+        Error!
+      </Flex>
+    )
   }
   // console.log(data)
 
