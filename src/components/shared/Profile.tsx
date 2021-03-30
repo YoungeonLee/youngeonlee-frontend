66e875eb6db6c1edba092ca0a70961f628b5c4c9
YoungeonLee <58812396+YoungeonLee@users.noreply.github.com>
2021-03-30 21:53:54 +0900

    breif intro

--- src/components/shared/Profile.tsx
+++ src/components/shared/Profile.tsx
@@ -1,18 +1,18 @@
 import { Avatar } from '@chakra-ui/avatar'
-import { Box, Divider } from '@chakra-ui/layout'
+import { Box, Divider, Flex } from '@chakra-ui/layout'
 
 export const Profile = () => {
   return (
     <>
       <Divider />
-      <Box my={1}>
+      <Flex my={2}>
         <Avatar mx="auto" size="2xl" name="Youngeon Lee" src="/profile.jpg" />
         <Box mx="auto">
           Hello, I'm Youngeon Lee. I will be sharing my side projects here, so
           feel free to check them out! My current interests are full-stack
           developement and data science.
         </Box>
-      </Box>
+      </Flex>
       <Divider />
     </>
   )
