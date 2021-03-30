5d135011dde14228c5b0e47e6044a1b67026fc27
YoungeonLee <58812396+YoungeonLee@users.noreply.github.com>
2021-03-30 21:41:29 +0900

    divider

--- src/components/shared/Profile.tsx
+++ src/components/shared/Profile.tsx
@@ -1,10 +1,14 @@
 import { Avatar } from '@chakra-ui/avatar'
-import { Box } from '@chakra-ui/layout'
+import { Box, Divider } from '@chakra-ui/layout'
 
 export const Profile = () => {
   return (
-    <Box mx="auto">
-      <Avatar size="2xl" name="Youngeon Lee" src="/profile.jpg" />
-    </Box>
+    <>
+      <Divider />
+      <Box>
+        <Avatar mx="auto" size="2xl" name="Youngeon Lee" src="/profile.jpg" />
+      </Box>
+      <Divider />
+    </>
   )
 }
