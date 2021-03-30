f8341bc89a86b43b528316ca1a003fc53e8a0596
YoungeonLee <58812396+YoungeonLee@users.noreply.github.com>
2021-03-30 20:44:35 +0900

    add my picture

--- src/components/shared/Profile.tsx
+++ src/components/shared/Profile.tsx
@@ -1,9 +1,11 @@
 import { Avatar } from '@chakra-ui/avatar'
 import { Box } from '@chakra-ui/layout'
+import { Image } from 'next/image'
 
 export const Profile = () => {
   return (
     <Box mx="auto">
+      <Image src="/profile-picture.jpg" alt="me" width="64" height="64" />
       <Avatar size="2xl" name="Segun Adebayo" src="/profile-picture.jpg" />
     </Box>
   )
