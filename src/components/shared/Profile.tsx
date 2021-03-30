4424b06cc848d00f878e76c84596eaf4d1e93fe4
YoungeonLee <58812396+YoungeonLee@users.noreply.github.com>
2021-03-30 21:37:35 +0900

    my initial

--- src/components/shared/Profile.tsx
+++ src/components/shared/Profile.tsx
@@ -1,12 +1,10 @@
 import { Avatar } from '@chakra-ui/avatar'
 import { Box } from '@chakra-ui/layout'
-import Image from 'next/image'
 
 export const Profile = () => {
   return (
     <Box mx="auto">
-      <Image src="/profile.jpg" alt="me" width="64" height="64" />
-      <Avatar size="2xl" name="Segun Adebayo" src="/profile.jpg" />
+      <Avatar size="2xl" name="Youngeon Lee" src="/profile.jpg" />
     </Box>
   )
 }
