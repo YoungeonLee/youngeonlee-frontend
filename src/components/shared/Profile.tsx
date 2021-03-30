b7ec37cde802ca4d0193cf86b09d0ae5ee4e5c4b
YoungeonLee <58812396+YoungeonLee@users.noreply.github.com>
2021-03-30 21:15:38 +0900

    debug

--- src/components/shared/Profile.tsx
+++ src/components/shared/Profile.tsx
@@ -5,8 +5,8 @@ import Image from 'next/image'
 export const Profile = () => {
   return (
     <Box mx="auto">
-      <Image src="/profile-picture.jpg" alt="me" width="64" height="64" />
-      <Avatar size="2xl" name="Segun Adebayo" src="/profile-picture.jpg" />
+      <Image src="/profile.jpg" alt="me" width="64" height="64" />
+      <Avatar size="2xl" name="Segun Adebayo" src="/profile.jpg" />
     </Box>
   )
 }
