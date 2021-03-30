e80f64c6f7b0aebaa2458fb597b9613ebd6f6241
YoungeonLee <58812396+YoungeonLee@users.noreply.github.com>
2021-03-30 22:08:52 +0900

    breif intro

--- src/components/shared/Profile.tsx
+++ src/components/shared/Profile.tsx
@@ -5,9 +5,9 @@ export const Profile = () => {
   return (
     <>
       <Divider />
-      <Flex my={2}>
+      <Flex my={2} alignItems="center">
         <Avatar mx="auto" size="2xl" name="Youngeon Lee" src="/profile.jpg" />
-        <Box ml={2} mr="auto">
+        <Box ml={3} mr="auto" fontSize="1.25rem">
           Hello, I'm Youngeon Lee. I will be sharing my side projects here, so
           feel free to check them out! My current interests are full-stack
           developement and data science. More projects coming!
